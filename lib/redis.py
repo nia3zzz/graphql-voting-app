d08418436386c619e8969a8c00a07862ff28dd20
nia3zzz <mushfiqurniaz33@gmail.com>
2025-11-17 21:01:53 +0600

    add redis connection

---
+++ lib/redis.py
@@ -0,0 +1,7 @@
+import redis
+
+# redis connection pool
+pool = redis.ConnectionPool(host="localhost", port=6379, db=0)
+
+# initilaize connection
+redisConnection = redis.Redis(connection_pool=pool, decode_responses=True)
